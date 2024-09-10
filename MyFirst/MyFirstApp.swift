3d1be0b583e434c9317237c66371485ef2c0a410
mahathirrojan <torbengalifreind@gmail.com>
2024-09-10 01:04:02 -0400

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Mahathir Rojan on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
